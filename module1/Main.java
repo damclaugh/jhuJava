f291c4c8ec55f533e7ec898032e5d028bdd9a950
David McLaughlin <damclaugh@gmail.com>
2023-08-30 16:52:38 -0400

    mod1 main

---
+++ module1/Main.java
@@ -0,0 +1,21 @@
+package module1;
+
+/*
+* This program prints "Java" where each letter is displayed using the character 
+* of that letter. It uses Java's escape sequences and spaces to line up the 
+* letters on four lines. It prints each line using Java's System.out.println 
+* method.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("\t\b\b\b\bJ\t A\t\b\bV\t\t\b\b\b\bV\tA");
+        System.out.println("\t\b\b\b\bJ\tA A\t\bV\t\b\b\b\b\bV\t\bA A");
+        System.out.println(
+                "J\t\b\b\b\bJ\t\bAAAAA\tV\t\b\b\b\b\b\bV\t\b\bAAAAA");
+        System.out.println(
+                " J J\t\b\bA\t\t\b\b\b\bA\t V\t\b\b\bA\t\t\b\b\b\b\bA");
+        // end main ()
+    }
+} // end class Main
